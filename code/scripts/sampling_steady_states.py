d5ede4800b30b033e6f994347366e655aef9996a
SotirisTouliopoulos <s.v.touliopoulos@gmail.com>
2025-04-11 10:23:19 +0200

    Started LoopLess Sampling testing

--- code/scripts/sampling_steady_states.py
+++ code/scripts/sampling_steady_states.py
@@ -1,13 +1,11 @@
-# %% this is an interactive chunk 
 
+import cobra
 from dingo import MetabolicNetwork, PolytopeSampler
 import numpy as np
 import pickle
 from cobra.io import load_json_model, read_sbml_model, load_matlab_model
 
-print("libraries loaded.")
 
-# %% 
 
 """
 #roseburia_dingo_model = MetabolicNetwork.from_mat("/home/touliopoulos/project/erasmus/erasmus_2025_project/data/Reconstructions_data/Roseburia_intestinalis_L1_82_withMucins.mat")
@@ -39,23 +37,42 @@ for i in (99, 99, 99):
     del steady_states
 """ 
 
-# %%
 
 
+"""
+modelfile = "/home/touliopoulos/project/erasmus/erasmus_2025_project/data/Reconstructions_data/Roseburia_intestinalis_L1_82_fixed.mat"
 
-roseburia_dingo_model = MetabolicNetwork.from_mat("/home/touliopoulos/project/erasmus/erasmus_2025_project/data/Reconstructions_data/Roseburia_intestinalis_L1_82_fixed.mat")
-reactions = roseburia_dingo_model.reactions
+roseburia_dingo_model = MetabolicNetwork.from_mat(modelfile)
+roseburia_dingo_model.set_opt_percentage(100)
 
-i = 0
-roseburia_dingo_model.set_opt_percentage(i)
-    
 sampler = PolytopeSampler(roseburia_dingo_model)
 steady_states = sampler.generate_steady_states()
+print(np.max(steady_states))
 
-file_name_samples = "/home/touliopoulos/project/erasmus/erasmus_2025_project/data/Steady_States_Samples/RI_no_mucins/RI_steady_states_opt_" + str(i) + "_rep_003.pckl"
+#file_name_samples = "/home/touliopoulos/project/erasmus/erasmus_2025_project/data/Steady_States_Samples/RI_no_mucins/RI_steady_states_opt_" + str(i) + "_rep_003.pckl"
+file_name_samples = "/home/touliopoulos/100_rep_3.pckl"
 
 with open(file_name_samples, "wb") as hopsy_samples_file: 
     pickle.dump(steady_states, hopsy_samples_file)
+"""
+
+
+
+from cobra.sampling import sample
+roseburia_cobra_model = load_matlab_model("/home/touliopoulos/project/erasmus/erasmus_2025_project/data/Reconstructions_data/Roseburia_intestinalis_L1_82_fixed.mat")
+samples_df = sample(roseburia_cobra_model, 20000)
+samples_np = samples_df.to_numpy(dtype=float)
+samples_np = samples_np.T
+
+print(samples_np.shape)
+print(np.max(samples_np[2944,]))
+print(np.min(samples_np[2944,]))
+print(np.max(samples_np))
+
+file_name_samples = "/home/touliopoulos/100_cobrapy_rep_3.pckl"
+
+with open(file_name_samples, "wb") as hopsy_samples_file: 
+    pickle.dump(samples_np, hopsy_samples_file)
 
 
 
