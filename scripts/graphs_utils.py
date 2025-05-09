38bf2a6e062fa9dd49882ebba6f24e0b2493769b
SotirisTouliopoulos <s.v.touliopoulos@gmail.com>
2025-05-09 16:48:43 +0200

    Organised project into scripts, tests and analysis

---
+++ scripts/graphs_utils.py
@@ -0,0 +1,67 @@
+
+import networkx as nx
+import numpy as np
+import plotly.graph_objects as go
+
+
+def construct_graph(correlation_matrix, cobra_model, remove_unconnected_nodes=False, correction=True):
+       
+    cobra_reactions_str = [str(reaction.id) for reaction in cobra_model.reactions]
+    
+    graph_matrix = correlation_matrix.copy()
+    np.fill_diagonal(graph_matrix, 0)
+    
+    if correction == True:
+        graph_matrix = abs(graph_matrix)
+        
+    G = nx.from_numpy_array(graph_matrix)
+    G = nx.relabel_nodes(G, lambda x: cobra_reactions_str[x])
+    
+    pos = nx.spring_layout(G)
+    unconnected_nodes = list(nx.isolates(G))
+
+    if remove_unconnected_nodes == True:
+        G.remove_nodes_from(unconnected_nodes)
+    
+    return G, pos
+
+
+def plot_graph(G, pos):
+    
+    fig = go.Figure()
+
+    for u, v, data in G.edges(data=True):
+        x0, y0 = pos[u]
+        x1, y1 = pos[v]
+        
+        edge_color = 'blue' if data['weight'] > 0 else 'red'
+        
+        fig.add_trace(go.Scatter(x=[x0, x1], y=[y0, y1], mode='lines', 
+                                    line=dict(width=abs(data['weight']) * 1, 
+                                    color=edge_color), hoverinfo='none',
+                                    showlegend=False))
+
+    for node in G.nodes():
+        x, y = pos[node]
+        node_name = G.nodes[node].get('name', f'Node {node}')
+  
+        fig.add_trace(go.Scatter(x=[x], y=[y], mode='markers', 
+                                    marker=dict(size=10),
+                                    text=[node_name],
+                                    textposition='top center',
+                                    name = node_name,
+                                    showlegend=False))
+        
+    fig.update_layout(width=800, height=800)
+    fig.show()
+    
+    
+def compare_betweenness_centralities(Graph_1, Graph_2):
+    
+    centrality_G1 = nx.betweenness_centrality(Graph_1, weight='weight', normalized=True)
+    centrality_G2 = nx.betweenness_centrality(Graph_2, weight='weight', normalized=True)
+    centrality_diff = {node: centrality_G2.get(node, 0) - centrality_G1.get(node, 0) for node in set(Graph_1.nodes()).union(Graph_2.nodes())}
+    sorted_nodes = sorted(centrality_diff.items(), key=lambda x: x[1], reverse=True)
+
+    for node, change in sorted_nodes:
+            print(f"Node {node}: ΔCentrality = {change:.4f}")
---
+++ scripts/load_modify_sample_utils.py
@@ -0,0 +1,189 @@
+
+from cobra.io import read_sbml_model
+from dingo import MetabolicNetwork, PolytopeSampler
+from cobra.sampling.optgp import OptGPSampler
+from cobra.util.solver import linear_reaction_coefficients
+from dingo import set_default_solver
+from gapsplit import gapsplit
+from cobra.flux_analysis.loopless import add_loopless
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+import pickle
+
+
+def load_model(filepath):
+    cobra_model = read_sbml_model(filepath)
+    cobra_reactions = cobra_model.reactions
+    
+    dingo_model = MetabolicNetwork.from_cobra_model(cobra_model)
+    dingo_reactions = dingo_model.reactions
+
+    return cobra_model, cobra_reactions, dingo_model, dingo_reactions
+
+
+def get_objective_functions(cobra_model):
+    objectives_dict = linear_reaction_coefficients(cobra_model)
+    objective_functions = list(objectives_dict.keys())
+    objective_functions_ids = [rxn.id for rxn in objective_functions]
+    
+    return objective_functions_ids
+
+
+def get_reaction_bounds(cobra_model):
+    reaction_bounds_dict = { }
+    reaction_ids = [ reaction.id for reaction in cobra_model.reactions ]
+                
+    for reaction_id in reaction_ids:
+        bounds = cobra_model.reactions.get_by_id(reaction_id).bounds
+        lb = round(bounds[0], 6)
+        up = round(bounds[1], 6)
+        
+        reaction_bounds_dict[reaction_id] = (lb, up)
+        
+    return reaction_bounds_dict
+
+
+def modify_model(cobra_model, objective_function="", optimal_percentage=100, reaction_bounds={}):
+    modified_cobra_model = cobra_model.copy()
+    
+    # first define reaction bounds from dictionary
+    if len(reaction_bounds) >= 1:
+        for reaction, rbounds in reaction_bounds.items():
+            rxn = modified_cobra_model.reactions.get_by_id(reaction)
+            rxn.lower_bound, rxn.upper_bound = rbounds
+        
+    # then modify object function and optimal percentage (this may overwrite bounds for objective function from previous dictionary)
+    modified_cobra_model.objective = objective_function
+    modified_cobra_model.reactions.get_by_id(objective_function).upper_bound = 1000
+    
+    fba_solution = modified_cobra_model.optimize()
+    objective = fba_solution.objective_value
+    
+    fraction_of_optimum = float(optimal_percentage / 100)
+    # in case due to numeric errors it gets a higher value
+    if fraction_of_optimum > 1:
+        fraction_of_optimum = 1
+    
+    # in case due to numeric errors it gets a higher value
+    minimum_objective_value = round( (objective * fraction_of_optimum), 6)
+    if minimum_objective_value > objective:
+        minimum_objective_value = minimum_objective_value
+        
+    # to avoid errors
+    if optimal_percentage == 100:
+        minimum_objective_value = minimum_objective_value - 1e-3
+    
+    modified_cobra_model.reactions.get_by_id(objective_function).lower_bound = minimum_objective_value
+    
+    modified_dingo_model = MetabolicNetwork.from_cobra_model(modified_cobra_model)
+    modified_dingo_model.set_opt_percentage(optimal_percentage)
+    
+    return modified_cobra_model, modified_dingo_model
+    
+
+def sample_optgp(cobra_model, n_samples = 3000, thinning = 100, reaction_in_rows = True):
+    sampler_optgp = OptGPSampler(cobra_model, thinning=thinning)
+    samples_optgp = sampler_optgp.sample(n=n_samples)
+    samples_optgp = samples_optgp.to_numpy()
+    
+    if reaction_in_rows == True:
+        samples_optgp = samples_optgp.T
+        
+    return samples_optgp
+    
+    
+def sample_dingo(dingo_model, reaction_in_rows = True, ess=1000, psrf = False):
+    set_default_solver("gurobi")
+
+    sampler_dingo_default = PolytopeSampler(dingo_model)
+    samples_dingo_default = sampler_dingo_default.generate_steady_states(ess=ess, psrf=psrf)
+    
+    if reaction_in_rows == True:
+        pass
+    else:
+        samples_dingo_default = samples_dingo_default.T
+        
+    return samples_dingo_default
+
+
+def sample_gapsplit(cobra_model, n_samples = 3000, reaction_in_rows = True, add_loopless_cobrapy = False, fraction_of_optimum=0):
+    
+    ec_cobra_model = cobra_model.copy()
+    
+    if add_loopless_cobrapy == True:
+        add_loopless(ec_cobra_model)
+    
+    gapsplit_samples = gapsplit(
+        ec_cobra_model, 
+        n=n_samples, 
+        gurobi_direct=False,
+        fraction_of_optimum=fraction_of_optimum
+    )
+    
+    gapsplit_samples = gapsplit_samples.to_numpy()
+    
+    if reaction_in_rows == True:
+        gapsplit_samples = gapsplit_samples.T
+        
+    return gapsplit_samples
+
+
+def export_to_pickle(samples, filename):
+    with open(filename, "wb") as hopsy_samples_file: 
+        pickle.dump(samples, hopsy_samples_file)
+
+
+def plot_grid_95_reactions(samples, cobra_model, nrows=20, ncols=5):
+    cobra_reactions_str = [str(reaction.id) for reaction in cobra_model.reactions]
+    samples = samples.copy()
+    
+    if nrows * ncols < len(cobra_reactions_str):
+        raise Exception("Change dimensions of the plot (nrows, ncols) to store all distributions")
+    
+    # if provided sampling dataset has reactions as rows ==> transpose
+    if samples.shape[0] == len(cobra_reactions_str):
+        samples = samples.T
+
+    df = pd.DataFrame(samples)
+    df.columns = cobra_reactions_str
+
+    nrows = nrows
+    ncols = ncols
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(4*ncols, 3*nrows))
+    axes = np.array(axes).flatten()
+
+    # Plot each column in its subplot
+    for i, col in enumerate(df.columns):
+        ax = axes[i]
+        ax.hist(df[col], bins=20, color='skyblue', edgecolor='black')
+        ax.set_title(col)
+        ax.grid(True)
+
+    # If there are unused axes (e.g., more grid spots than columns), hide them
+    for j in range(len(df.columns), len(axes)):
+        fig.delaxes(axes[j])  # or: axes[j].axis('off')
+
+    plt.tight_layout()
+    plt.show()
+    
+
+def sampling_statistics(samples, reactions_ids_list=None, reaction_id=""):
+    if reactions_ids_list != None:
+        raise Exception("List with Reactions IDs not provided")
+        
+    if reaction_id in reactions_ids_list:
+        reaction_index = reactions_ids_list.index(reaction_id)
+    
+        mean = np.mean(samples[reaction_index])
+        min = np.min(samples[reaction_index]) 
+        max = np.max(samples[reaction_index])
+        std = np.std(samples[reaction_index])
+        skewness = stats.skew(samples[reaction_index])
+        kurtosis = stats.kurtosis(samples[reaction_index])
+    
+        return mean, min, max, std, skewness, kurtosis
+    
+    else:
+        print("Reaction ID provided not in the list of Reactions IDs")
